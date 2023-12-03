d8c8ff2950c45d709a88234f9ec0a9ecc107c272
ellocoavs <el_loco_avs@hotmail.com>
2023-12-03 10:44:32 +0100

    i+2 and i+3 need to separated still
    for one two six separate from
    four five nine
    yesyes

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -13,7 +13,7 @@ public class Day1 {
     static boolean test = true;
     static String day = "Day1";
     static int testanswer1 = 142;
-    static int testanswer2 = 281;
+    static int testanswer2 = 351;
 
     public static void main(String[] args) {
         filename = Tools.setInputfile(test, day);
@@ -32,7 +32,9 @@ public class Day1 {
             }
         }
         */
-        int answer2 = part2(input);
+        int answer2 = part2(input);        
+        System.out.println("Answer2 is: " + Integer.toString(answer2));
+
         if (test){
             if (testanswer2 == answer2){
                 System.out.println("Test for part 2 succesful");
@@ -105,7 +107,7 @@ public class Day1 {
             first = null;
             last = null;
             for (int i=0; i<current.length();i++){
-                System.out.println("Numbers are now: "+first+last);
+                //System.out.println("Numbers are now: "+first+last);
                 if (first == null && StringUtils.isNumeric(current.substring(i,i+1))){
                     first = current.substring(i,i+1);
                     last = first;
@@ -114,61 +116,73 @@ public class Day1 {
                 }
                 //if next five chars contain a spelled out number...
                 else if (first == null){
-                    if (current.substring(i, i+4).contains(nums.get(1))){
-                        first = "1";
-                        last = first;
-                        i=i+2;
-                        continue;
+                    if (i+4 < current.length()){   
+                        if (current.substring(i, i+5).contains(nums.get(3))){
+                            first = "3";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+3;
+                            continue;
+                        }
+                            if (current.substring(i, i+5).contains(nums.get(7))){
+                            first = "7";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+3;
+                            continue;
+                        }
+                        if (current.substring(i, i+5).contains(nums.get(8))){
+                            first = "8";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+3;
+                            continue;
+                        }
                     }
-                    if (current.substring(i, i+4).contains(nums.get(2))){
-                        first = "2";
-                        last = first;
-                        i=i+2;
-                        continue;
-                    }
-                    if (current.substring(i, i+5).contains(nums.get(3))){
-                        first = "3";
-                        last = first;
-                        i=i+3;
-                        continue;
-                    }
-                    if (current.substring(i, i+4).contains(nums.get(4))){
-                        first = "4";
-                        last = first;
-                        i=i+2;
-                        continue;
-                    }
-                    if (current.substring(i, i+4).contains(nums.get(5))){
-                        first = "5";
-                        last = first;
-                        i=i+2;
-                        continue;
-                    }
-                    if (current.substring(i, i+4).contains(nums.get(6))){
-                        first = "6";
-                        last = first;
-                        i=i+2;
-                        continue;
-                    }
-                    if (current.substring(i, i+5).contains(nums.get(7))){
-                        first = "7";
-                        last = first;
-                        i=i+3;
-                        continue;
-                    }
-                    if (current.substring(i, i+5).contains(nums.get(8))){
-                        first = "8";
-                        last = first;
-                        i=i+5;
-                        continue;
-                    }
-                    if (current.substring(i, i+4).contains(nums.get(9))){
-                        first = "9";
-                        last = first;
-                        i=i+2;
-                        continue;
-                    }
-                
+                    if (i+3 < current.length()){
+                        if (current.substring(i, i+4).contains(nums.get(1))){
+                            first = "1";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+1;
+                            continue;
+                            }
+                        if (current.substring(i, i+4).contains(nums.get(2))){
+                            first = "2";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+1;
+                            continue;
+                        }
+                        if (current.substring(i, i+4).contains(nums.get(4))){
+                            first = "4";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+2;
+                            continue;
+                        }
+                        if (current.substring(i, i+4).contains(nums.get(5))){
+                            first = "5";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+2;
+                            continue;
+                        }
+                        if (current.substring(i, i+4).contains(nums.get(6))){
+                            first = "6";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+1;
+                            continue;
+                        }
+                        if (current.substring(i, i+4).contains(nums.get(9))){
+                            first = "9";
+                            System.out.println("First number is: "+first);
+                            last = first;
+                            i=i+2;
+                            continue;
+                        }   
+                    } 
                 }
                 else { //just checking for last number.
                     if (StringUtils.isNumeric(current.substring(i,i+1))){
@@ -177,66 +191,77 @@ public class Day1 {
                         continue;
                     }
                     
-                    if (i+3 >= current.length()){break;}
-                    System.out.println("Now peeking at: " + current.substring(i, i+3));
+                    if (i+2 >= current.length()){continue;}
+                    System.out.println("Now peeking at: " + current.substring(i, i+2));
                     if (i+4 < current.length()){                   
                         if (current.substring(i, i+5).contains(nums.get(3))){
                             last = "3";
-                            i=i+4;
+                            System.out.println("Last number is: "+last);
+                            i=i+3;
                             continue;
                         }
                    
                         if (current.substring(i, i+5).contains(nums.get(7))){
                             last = "7";
-                            i=i+4;
+                            System.out.println("Last number is: "+last);
+                            i=i+3;
                             continue;
                         }
                         if (current.substring(i, i+5).contains(nums.get(8))){
                             last = "8";
-                            i=i+4;
+                            System.out.println("Last number is: "+last);
+                            i=i+3;
                             continue;
                         }
                      
                     }
-                        //SOMETIMES WE DONT GET HER?E?!!?!? xtwone3four
-                    else if (i+3 < current.length()){                 
+                        
+                    if (i+3 < current.length()){                 
                         if (current.substring(i, i+4).contains(nums.get(1))){
                             last = "1";
-                            i=i+3;
+                            System.out.println("Last number is: "+last);
+                            i=i+1;
                             continue;
                         }
                         if (current.substring(i, i+4).contains(nums.get(2))){
                             last = "2";
-                            i=i+3;
+                            System.out.println("Last number is: "+last);
+                            i=i+1;
                             continue;
                         }
                         if (current.substring(i, i+4).contains(nums.get(4))){
                             last = "4";
-                            i=i+3;
+                            System.out.println("Last number is: "+last);
+                            i=i+2;
                             continue;
                         }
                         if (current.substring(i, i+4).contains(nums.get(5))){
                             last = "5";
-                            i=i+3;
+                            System.out.println("Last number is: "+last);
+                            i=i+2;
                             continue;
                         }
                         if (current.substring(i, i+4).contains(nums.get(6))){
                             last = "6";
-                            i=i+3;
+                            System.out.println("Last number is: "+last);
+                            i=i+1;
                             continue;
                         }
                         if (current.substring(i, i+4).contains(nums.get(9))){
                             last = "9";
-                            i=i+3;
+                            System.out.println("Last number is: "+last);
+                            i=i+2;
                             continue;
                         }
                     }   
                 }
-                System.out.println("Numbers are now: "+first+last);
+                //System.out.println("Numbers are now: "+first+last);
             }
-            System.out.println("Numbers are now: "+first+last);
+            System.out.println("Adding: " +first+last + " to runningtotal");
+            runningtotal = runningtotal + Integer.parseInt(first+last);
+
         }
-        System.out.println("Numbers are now: "+first+last);
+        //System.out.println("Numbers are now: "+first+last);
         answer = runningtotal;
         return answer;
     }
